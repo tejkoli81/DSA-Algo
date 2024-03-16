cff93eaba3d89940e2069e8aa3776613aa6e2ab2
Tejkumar Koli <tejkoli81@gmail.com>
2024-03-16 23:29:39 +0530

    Initial commit

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=10;i++){
+            if(((i*i*i)/(i+i+i))==i){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
