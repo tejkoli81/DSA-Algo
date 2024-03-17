3f05a05ea6a4a2368b647133131bee91c47d60ad
Tejkumar Koli <tejkoli81@gmail.com>
2024-03-17 11:14:33 +0530

    bitwise AND

---
+++ Bit_manpulation/bitwise_AND.java
@@ -0,0 +1,9 @@
+package Bit_manpulation;
+
+public class bitwise_AND {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        System.out.println(a & b);
+    }
+}
