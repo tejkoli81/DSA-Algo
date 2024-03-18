6e6df870c13933421229093c4d6260f884d64e67
Tejkumar Koli <tejkoli81@gmail.com>
2024-03-18 16:44:12 +0530

    recursion

---
+++ recursion/check_array_is_sorted.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class check_array_is_sorted {
+
+    public static boolean isSorted(int arr[], int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        System.out.println("Is the array sorted? :" + isSorted(arr,0));
+    }
+}
