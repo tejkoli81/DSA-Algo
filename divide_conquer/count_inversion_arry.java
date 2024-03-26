f4837a74c0dea451bc57a9592eb9ad1ad532507e
Tejkumar Koli <tejkoli81@gmail.com>
2024-03-26 23:13:32 +0530

    divide and conqour

---
+++ divide_conquer/count_inversion_arry.java
@@ -0,0 +1,19 @@
+package divide_conquer;
+
+public class count_inversion_arry {
+    public static int countinversion(int arr[]){
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int arr[]={5,2,6,1};
+        System.out.println(countinversion(arr));
+    }
+}
