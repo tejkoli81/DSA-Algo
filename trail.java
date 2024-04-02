619fa70ab4134c9a05a8b5a86ffed26de73dcd19
Tejkumar Koli <tejkoli81@gmail.com>
2024-04-02 22:46:18 +0530

    backtracking

--- trail.java
+++ trail.java
@@ -1,9 +1,44 @@
 class trail{
+    static int count=0;
+    public static void solveNQ(char board[][],int rows){
+        //base case
+        if(rows == board.length){
+            count++;
+            return;
+        }
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,rows,j)){
+            board[rows][j]='Q';
+            solveNQ(board, rows+1);
+            board[rows][j]='x'; // undo the assignment if it doesn't work out;
+        }
+    }
 
-public static void main (String args[]){
+    }
+    public static boolean isSafe(char[][] board, int row ,int col){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1, j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1, j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    
 
-System.out.println("This is trail file");
-
-}
-
+    public static void main(String[] args) {
+        int n=1;
+        char board [][]=new char[n][n];
+        solveNQ(board,0);
+        System.out.println("no of ways: "+count);
+    }
 }
