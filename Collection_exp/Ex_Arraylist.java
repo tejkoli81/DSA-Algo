816f275abbf6c9c8958c8c8d3fd79f215dcf2ca0
Tejkumar Koli <tejkoli81@gmail.com>
2024-09-15 14:50:32 +0530

    Collections

---
+++ Collection_exp/Ex_Arraylist.java
@@ -0,0 +1,12 @@
+package Collection_exp;
+import java.util.*;
+
+public class Ex_Arraylist {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        System.out.println(list);
+    }
+}
